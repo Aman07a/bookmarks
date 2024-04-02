587e8a905026d3f905ac8a4b7ef358f797c1648a
Aman07a <aman16.aa17@gmail.com>
2024-04-02 10:05:34 +0200

    Updated Book & Review from Migration

--- db/migrate/20240402080049_create_reviews.rb
+++ db/migrate/20240402080049_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
+      t.string :author
+      t.string :comment
+      t.references :book, null: false, foreign_key: true
 
       t.timestamps
     end
